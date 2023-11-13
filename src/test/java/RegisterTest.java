0b3670ace7ff03ebd899b7b046fd2525b38a3ed4
Carla Antonini <carla.antonini@hotmail.com>
2023-11-13 15:40:02 -0300

    eliminar RegistroMailRepetidoTest por no completado de datos en RegisterTest

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -31,7 +31,6 @@ public class RegisterTest {
         registerPage.getUrl("https://opencart.abstracta.us/index.php?route=common/home");
     }
 
-
     @Test
     @Tag("Registro")
     @Tag("ALL")
@@ -65,57 +64,12 @@ public class RegisterTest {
             /*registerPage.clickContinuarLogin();
             test.log(Status.PASS, "Regreso a la página inicial");*/
 
-
         } catch (AssertionError error) {
             test.log(Status.FAIL, "Fallo la validación: " + error.getLocalizedMessage());
             throw error;
         }
     }
 
-
-
-    /*
-    @Test
-    @Tag("Registro")
-    @Tag("ALL")
-    public void   RegistroMailRepetidoTest() throws InterruptedException {
-        ExtentTest test = extent.createTest("Prueba de Crear una cuenta con un mail repetido");
-        test.log(Status.INFO, "Comienza el Test");
-        RegisterPage registerPage = new RegisterPage(driver, wait);
-
-        try {
-            registerPage.clickMiCuenta();
-            registerPage.clickRegistrar();
-            Assertions.assertEquals(registerPage.obtenerTituloRegistro(), "Account");
-            test.log(Status.PASS, "Ingreso a la página de Registro");
-
-            registerPage.escribirNombre("Carla Gisele");
-            registerPage.escribirApellido("Antonini");
-            registerPage.escribirMail("carla.antonini@hotmail.com");
-            registerPage.escribirTelefono("1161112222");
-            registerPage.escribirContrasenia("123456");
-            registerPage.escribirConfirmarContrasenia("123456");
-
-            registerPage.clickAceptarPoliticasPrivacidad();
-
-            test.log(Status.PASS, "Completo los datos de registro de forma correcta, con un mail usado");
-
-            registerPage.clickRegistrarse();
-            test.log(Status.PASS, "Completo el registro");
-
-            Assertions.assertEquals(registerPage.obtenerMensajeCorreoRepetido(), "Warning: E-Mail Address is already registered!");
-            test.log(Status.PASS, "Valido no poder registrarme con un correo repetido");
-
-            test.log(Status.PASS, "Regreso a la página inicial");
-
-            driver.manage().deleteAllCookies();
-            driver.navigate().refresh();
-        } catch (AssertionError error) {
-            test.log(Status.FAIL, "Fallo la validación: " + error.getLocalizedMessage());
-            throw error;
-        }
-    }
-*/
     @AfterEach
     public void cerrar() {
         RegisterPage registerPage = new RegisterPage(driver, wait);
